17f9ed249dcad1bb376c21682bb095f6cf0f3d8d
Cezar Moreira <cezargithub@gmail.com>
2025-11-06 22:12:58 -0300

    criação do main controller e da função index que direciona para a view home

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+
+    //função index para retornar a view welcome
+    public function index()
+    {
+        return view('home');
+    }
+}
