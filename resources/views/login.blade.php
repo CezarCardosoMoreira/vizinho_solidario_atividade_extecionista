325c052e6dc0cb80225d554aeedec155975b0098
Cezar Moreira <cezargithub@gmail.com>
2025-11-08 20:30:06 -0300

    mostra erros não preencher o formulario

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,12 +10,22 @@
             @csrf
             <div class="input-group">
                 <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" required placeholder="seu.email@exemplo.com">
+                <input type="email" id="email" name="email" required  placeholder="seu.email@exemplo.com" value="{{ old('email') }} ">
+
+                {{-- show error --}}
+                @error('email')
+                    <div class="alert-danger">{{ $message }}</div>
+
+                @enderror
             </div>
 
             <div class="input-group">
                 <label for="senha">Senha:</label>
-                <input type="password" id="senha" name="senha" required placeholder="Digite sua senha">
+                <input type="password" id="senha" name="senha" required placeholder="Digite sua senha" >
+                @error('senha')
+                    <div class="alert-danger">{{ $message }}</div>
+
+                @enderror
             </div>
 
             <button type="submit" class="login-button">Entrar</button>
@@ -24,7 +34,14 @@
         <div class="signup-option">
             <p>Novo por aqui? <a href="/sua-pagina-de-cadastro">**Cadastre-se**</a></p>
         </div>
+        <div>Vizinho Solidario  &copy;</div>
+        {{-- Mensagem de erro --}}
+
+
     </div>
+
+
+
 >
 
 @endsection
