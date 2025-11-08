a84461b7784998f76d2fcc4fa03001670647f9b1
Cezar Moreira <cezargithub@gmail.com>
2025-11-08 18:29:19 -0300

    adiciona o css e o footer

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -4,16 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-
     <title>Document</title>
 </head>
 <body>
     @yield('content')
 
-    <br><br>
-    <div class="bg-black text-white p3 text-center mt-5">
-        [ Vizinhos Solidarios    &copy;   {{ date('Y') }} ]
-    </div>
 </body>
 </html>
